15b250dfe8b1e437c5e45a1146a44997581f404f
Victor Remmerswaal <victorr@blackgate.nl>
2016-03-08 11:55:50 +0100

    Models weer toegevoegd

---
+++ ProjectBlokH/ProjectBlokH/Models/Reservation.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace projectshizzleblokh.Models
+{
+    public class Reservation
+    {
+        public DateTime Date { get; set; }
+        public Restaurant Restaurant { get; set; }
+    }
+}
