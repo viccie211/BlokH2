3c04f0f5d0d2373bfaaf427d699e85dda57f4664
mitchelh <mitchelhulst@gmail.com>
2016-03-08 12:47:09 +0100

    Updated IApplicationRepository

--- ProjectBlokH/ProjectBlokH/Models/IApplicationRepository.cs
+++ ProjectBlokH/ProjectBlokH/Models/IApplicationRepository.cs
@@ -7,17 +7,25 @@ namespace ProjectBlokH.Models
 {
     public interface IApplicationRepository
     {
-        IEnumerable<Reservation> GetAll();
+        IEnumerable<Reservation> GetAllReservations();
+        IEnumerable<User> GetAllUsers();
+        IEnumerable<Restaurant> GetAllRestaurants();
+
         Reservation GetReservation(int id);
         Reservation AddReservation(Reservation reservation);
+
         User AddUser(User user);
         User GetUser(int id);
+
         Restaurant AddRestaurant(Restaurant restaurant);
         Restaurant GetRestaurant(int id);
+
         bool UpdateReservation(Reservation reservation);
         void RemoveReservation(int id);
+
         bool UpdateUser(User user);
         void RemoveUser(int id);
+
         bool UpdateRestaurant(Restaurant restaurant);
         void RemoveRestaurant(int id);
     }
