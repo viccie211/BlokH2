5b3b3e0561a1709a8f96a492c48cc8d6bfa23f93
Victor Remmerswaal <victorr@blackgate.nl>
2016-03-08 12:57:03 +0100

    Reservation id en user toegevoegd

--- ProjectBlokH/ProjectBlokH/Models/Reservation.cs
+++ ProjectBlokH/ProjectBlokH/Models/Reservation.cs
@@ -7,7 +7,9 @@ namespace ProjectBlokH.Models
 {
     public class Reservation
     {
+        public int id { get; set; }
         public DateTime Date { get; set; }
         public Restaurant Restaurant { get; set; }
+        public User User { get; set; }
     }
 }
