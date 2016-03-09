6b6b92f4ba6942252007e2b51ea872e5acb9de20
mitchelh <mitchelhulst@gmail.com>
2016-03-09 14:41:28 +0100

    removed merge syntax

--- ProjectBlokH/ProjectBlokH/Controllers/ReservationController.cs
+++ ProjectBlokH/ProjectBlokH/Controllers/ReservationController.cs
@@ -4,23 +4,16 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
-<<<<<<< .merge_file_a16536
-=======
 using ProjectBlokH.Models;
->>>>>>> .merge_file_a16528
 
 namespace ProjectBlokH.Controllers
 {
     public class ReservationController : ApiController
     {
-<<<<<<< .merge_file_a16536
-
-=======
         public IEnumerable<Reservation> getReservations()
         {
             IApplicationRepository data = new ApplicationReadRepository();
             return data.GetAllReservations();
         }
->>>>>>> .merge_file_a16528
     }
 }
